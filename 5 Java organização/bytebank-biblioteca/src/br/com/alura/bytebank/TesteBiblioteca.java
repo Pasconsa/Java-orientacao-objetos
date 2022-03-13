a963f923b469282c98f6459adf81768c7916a263
Pasconsa <98270728+Pasconsa@users.noreply.github.com>
2022-03-13 16:10:23 -0300

    Organização de package

---
+++ 5 Java organização/bytebank-biblioteca/src/br/com/alura/bytebank/TesteBiblioteca.java
@@ -0,0 +1,14 @@
+package br.com.alura.bytebank;
+
+import br.com.Bytebank.Banco.Modelo.Conta;
+import br.com.Bytebank.Banco.Modelo.ContaCorrente;
+
+public class TesteBiblioteca {
+	
+	public static void main(String[] args) {
+		
+		Conta c = new ContaCorrente(123, 321);
+		
+	}
+
+}

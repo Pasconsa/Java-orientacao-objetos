9901c65b7f9dda5806a7442052e330cfad5b11f1
Pasconsa <98270728+Pasconsa@users.noreply.github.com>
2022-03-28 18:04:15 -0300

    parte 7

---
+++ 5 Java organização/java-pilha/src/TesteObject.java
@@ -0,0 +1,23 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteObject {
+	public static void main(String[] args) {
+		
+		//[] array usar arrays java
+		
+		int[] idades = new int [5]; //inicializa o array com  zero
+		
+		idades[0] = 29 ;
+		idades[1] = 39 ; 
+		idades[2] = 49 ; 
+		idades[3] = 59 ; 
+		idades[4] = 69 ; 
+				
+		int idadeMostra = idades[4];     
+		
+				System.out.println(idadeMostra);
+				System.out.println(idades.length);
+	}
+
+	
+}
